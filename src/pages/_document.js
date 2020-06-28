011ee110f3220588b8694fd984b1f9738bc0dc75
Felipe Urbanski <felipeurbansk@gmail.com>
2020-06-28 06:17:33 -0300

    Change layout about me

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -20,18 +20,6 @@ class MyDocument extends Document {
             content="Essa é a minha página web, aqui você vai encontrar dados para contato, skills, portifólio e redes sociais."
             key="description"
           />
-          <link
-            rel="preload"
-            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;1,900&display=swap"
-            as="font"
-            crossOrigin=""
-          />
-          <link
-            rel="preload"
-            href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;700&display=swap"
-            as="font"
-            crossOrigin=""
-          />
           <NavBar />
         </Head>
         <body>
--- src/pages/components/SectionAbout.js
+++ src/pages/components/SectionAbout.js
@@ -1,67 +1,107 @@
-import { FiInfo, FiHeadphones, FiCoffee, FiCode, FiCpu } from "react-icons/fi";
-import { FaDog } from "react-icons/fa";
+import { FiHeadphones, FiCoffee, FiCode, FiCpu } from "react-icons/fi";
+import { MdMovie } from "react-icons/md";
 import { GiGamepad } from "react-icons/gi";
+import {
+  DiPhp,
+  DiHtml5,
+  DiCss3,
+  DiJavascript1,
+  DiNodejsSmall,
+  DiReact,
+  DiBootstrap,
+  DiGithubBadge,
+  DiLaravel,
+  DiNpm,
+  DiComposer,
+  DiWordpress,
+} from "react-icons/di";
 
 const SectionAbout = () => (
   <section className="content-about">
-    <h3>About me</h3>
-    {/* <FiInfo size={25} /> */}
-    {/* <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the
-      1500s, when an unknown printer took a galley of type and scrambled it to
-      make a type specimen book.
-    </p> */}
     <div className="group-columns">
-      <div className="colum contact">
-        <h4>Meus Contatos</h4>
-        <table className="table-contact">
-          <tbody>
-            <tr>
-              <th>Nascimento:</th>
-              <td>19/10/1993</td>
-            </tr>
-            <tr>
-              <th>Phone:</th>
-              <td>(41) 98508-1756</td>
-            </tr>
-            <tr>
-              <th>Email:</th>
-              <td>felipeurbansk@gmail.com</td>
-            </tr>
-            <tr>
-              <th>Residencia:</th>
-              <td>Curitiba, PR</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div className="colum my-interest">
-        <div className="frame-interest">
-          <GiGamepad />
-          <span>Games</span>
+      <fieldset>
+        <legend>Experiencia</legend>
+        <div className="colum experiences">
+          <div className="frame-icon php">
+            <DiPhp />
+            <span>PHP</span>
+          </div>
+          <div className="frame-icon">
+            <DiLaravel />
+            <span>Laravel</span>
+          </div>
+          <div className="frame-icon">
+            <DiHtml5 />
+            <span>HTML5</span>
+          </div>
+          <div className="frame-icon">
+            <DiCss3 />
+            <span>CSS3</span>
+          </div>
+          <div className="frame-icon">
+            <DiJavascript1 />
+            <span>Javascript</span>
+          </div>
+          <div className="frame-icon">
+            <DiWordpress />
+            <span>Wordpress</span>
+          </div>
+          <div className="frame-icon">
+            <DiNodejsSmall />
+            <span>Node</span>
+          </div>
+          <div className="frame-icon">
+            <DiReact />
+            <span>React</span>
+          </div>
+          <div className="frame-icon">
+            <DiBootstrap />
+            <span>Bootstrap</span>
+          </div>
+          <div className="frame-icon">
+            <DiGithubBadge />
+            <span>Github</span>
+          </div>
+          <div className="frame-icon">
+            <DiNpm />
+            <span>NPM</span>
+          </div>
+          <div className="frame-icon">
+            <DiComposer />
+            <span>Composer</span>
+          </div>
         </div>
-        <div className="frame-interest">
-          <FiHeadphones />
-          <span>Musica</span>
+      </fieldset>
+      <fieldset>
+        <legend>Interesses</legend>
+        <div className="colum my-interest">
+          {/* <legend className="title-columns">Interest</legend> */}
+          <div className="frame-icon">
+            <GiGamepad />
+            <span>Games</span>
+          </div>
+          <div className="frame-icon">
+            <FiHeadphones />
+            <span>Musica</span>
+          </div>
+          <div className="frame-icon">
+            <FiCoffee />
+            <span>Coffe</span>
+          </div>
+          <div className="frame-icon">
+            <FiCode />
+            <span>Code</span>
+          </div>
+          <div className="frame-icon">
+            <FiCpu />
+            <span>Technology</span>
+          </div>
+          <div className="frame-icon">
+            <MdMovie />
+            <span>Movies</span>
+          </div>
         </div>
-        <div className="frame-interest">
-          <FiCoffee />
-          <span>Coffe</span>
-        </div>
-        <div className="frame-interest">
-          <FiCode />
-          <span>Code</span>
-        </div>
-        <div className="frame-interest">
-          <FiCpu />
-          <span>Technology</span>
-        </div>
-        <div className="frame-interest">
-          <FaDog />
-          <span>Animals</span>
-        </div>
-      </div>
+      </fieldset>
     </div>
   </section>
 );
