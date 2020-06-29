eb962c4da40b00745697e03d216f85b5a47ea155
Felipe Urbanski <felipeurbansk@gmail.com>
2020-06-29 01:03:06 -0300

    Fix height sections

--- src/pages/components/SectionFooter.js
+++ src/pages/components/SectionFooter.js
@@ -1,23 +1,26 @@
 import moment from "moment";
-import { FiTwitter, FiFacebook, FiLinkedin } from "react-icons/fi";
+import { FiTwitter, FiFacebook, FiLinkedin, FiGithub } from "react-icons/fi";
 
 const SectionFooter = () => (
   <footer className="content-footer">
     <div className="social">
       <ul>
         <li>
-          <FiTwitter />
+          <FiGithub />
+        </li>
+        <li>
+          <FiLinkedin />
         </li>
         <li>
           <FiFacebook />
         </li>
         <li>
-          <FiLinkedin />
+          <FiTwitter />
         </li>
       </ul>
     </div>
     <hr className="divider-footer" />
-    <small class="text-muted">
+    <small class="text-white">
       Felipe Urbanski -{" "}
       <cite title="Source Title">{moment().format("YYYY")}</cite>
     </small>
