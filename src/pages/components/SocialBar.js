3f5c1fe0f37c13500ae12232d0069e590547c383
Felipe Urbanski <felipeurbansk@gmail.com>
2024-06-16 19:00:48 -0300

    fix: update linkedin url

--- src/pages/components/SocialBar.js
+++ src/pages/components/SocialBar.js
@@ -4,7 +4,7 @@ import { AiFillGithub } from "react-icons/ai";
 const SocialBar = () => (
   <div className="social">
     <ul>
-      <a href="https://www.linkedin.com/in/felipeurbanski/" target="_blank">
+      <a href="https://www.linkedin.com/in/felipeurbansk/" target="_blank">
         <li>
           <FiLinkedin />
         </li>
